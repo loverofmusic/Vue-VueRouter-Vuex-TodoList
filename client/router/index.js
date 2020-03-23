60487ce9478c343c2247b733603b26ffc8bb4ef5
loverofmusic <1073972000@qq.com>
2020-03-23 13:55:15 +0800

    配置路由参数

--- client/router/index.js
+++ client/router/index.js
@@ -5,9 +5,26 @@ import routes from "./routes";
 export default () => {
   return new Router({
     routes,
-    mode: 'history',
+    mode: "history",
     // base: '/base/',
-    linkActiveClass: 'active-link',
-    linkExactActiveClass: 'exact-active-link'
+    // linkActiveClass: 'active-link',
+    // linkExactActiveClass: 'exact-active-link',
+    scrollBehavior(to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition;
+      } else {
+        return {
+          x: 0,
+          y: 0
+        };
+      }
+    },
+    // parseQuery(query){
+
+    // },
+    // stringifyQuery(obj){
+
+    // }
+    // fallback: true //在不支持history模式的浏览器里，如果设置为false，则变成多页应用，每次路由变了就重新请求server；如果设置为true则变成hash模式
   });
 };
